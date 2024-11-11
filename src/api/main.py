c986d6b6846960451fdca0830c4a3d3d6533d54e
vkz <vkz.work@protonmail.com>
2024-11-11 22:45:34 +0000

    update logic, update gitignore

--- src/api/main.py
+++ src/api/main.py
@@ -1,29 +1,17 @@
 import logging
 
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 
-from src.api.pinecone_helpers import get_pinecone_client
+from src.api.main_helpers import search_similar_sentences
 
 
 app = FastAPI()
-pc_handle = get_pinecone_client()
 
-class SearchResponse(BaseModel):
-    id: str
-    text: str
-    score: float
 
 @app.get("/search")
 async def search(query: str, top_k: int = 3):
     try:
-        results = pc_handle.search(query, top_k=top_k)
-
-        return {"results": [
-            SearchResponse(id=result["id"], 
-                           text=result["metadata"]["text"],
-                           score=result["score"],)
-        for result in results["matches"]]}
+        return search_similar_sentences(query, top_k)
     except Exception as e:
         logging.error(f"Error searching in Pinecone: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
--- src/api/pinecone_helpers.py
+++ src/api/pinecone_helpers.py
@@ -3,18 +3,7 @@ from pinecone import Pinecone, QueryResponse, Index
 from sentence_transformers import SentenceTransformer
 
 from src.api.settings import settings
-
-
-def get_pinecone_client():
-    return PineconeHandle()
-
-
-class SentenceVectorizator:
-    def __init__(self, model_name_or_path:str = settings.transformers_sentence_model_name) -> None:
-        self.model: SentenceTransformer = SentenceTransformer(model_name_or_path)
-
-    def vectorize_query(self, query: str) -> list[float]:
-        return [float(t) for t in self.model.encode(query)]
+from src.api.vectorize import SentenceVectorizator
 
 
 class PineconeHandle:
